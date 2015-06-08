dcf35c312bbfa44bcdf189d1533614dfc7c63945
hezhenjie <hezhenjie@xiaoma.cn>
2015-06-08 11:22:26 +0800

    Initial Commit

---
+++ PracticeConstraint3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PracticeConstraint3
+//
+//  Created by HZJ on 15/6/8.
+//  Copyright (c) 2015年 HZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PracticeConstraint3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticeConstraint3
+//
+//  Created by HZJ on 15/6/8.
+//  Copyright (c) 2015年 HZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
